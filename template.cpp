3fc9459fd4eb81b28a9aa77a0ee245b9aca82fe7
teksmz <teksmz@gmail.com>
2016-05-05 23:32:49 +0900

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+#include <string>
+#include <sstream>
+
+#define For(i, j) for(int i = 0; i < j; i++)
+
+using namespace std;
+
+int main()
+{
+  return 0;
+}

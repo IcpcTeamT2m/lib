c82cc558524cb19bdd3d2e339226ecf47d911770
teksmz <teksmz@gmail.com>
2016-05-07 20:36:23 +0900

    Add

--- STL (Standard Template Library)/map.cpp
+++ STL (Standard Template Library)/map.cpp
@@ -4,3 +4,14 @@
 // 宣言
 
 #include <map>
+
+typedef map(型, 型) name;
+typedef map(string, int) name; //keyがstring型、値がint型
+
+
+//例
+
+name.insert(map<string, int>::value_type(10, "wzken")); //key 10 に "wzken" を格納
+
+name.clear(); // 要素の全消去
+name.empty(); // 空ならTrueを返す

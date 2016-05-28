84b7de8f476faf06143a13c25781c8e6603d00fa
omu <omuric@users.noreply.github.com>
2016-05-28 13:19:18 +0900

    Update map.cpp

--- How-to/STL (Standard Template Library)/map.cpp
+++ How-to/STL (Standard Template Library)/map.cpp
@@ -18,3 +18,34 @@ int a = name["wzken"];//aに10が格納される
 name.clear(); // 要素の全消去
 name.empty(); // 空ならTrueを返す
 
+//値を格納すると自動的にkeyの昇順で内部がソートされる
+map<int,int> name2;
+name2[1] = 2;
+name2[5] = 5;
+name2[2] = 10;
+
+//内部の順番
+/*
+  key 1 : value  2
+  key 2 : value 10
+  key 5 : value  5
+*/
+
+//mapの中身を走査
+for(auto i : name2){
+  //key   = i.first
+  //value = i.second
+  cout << i.first << " " << i.second << endl;
+}
+
+//出力結果
+/*
+  1 2
+  2 10
+  5 5
+*/
+
+
+
+
+

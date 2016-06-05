51b77350549dc92a67536261c2c6b0a66ccbd036
omu <omuric@users.noreply.github.com>
2016-06-05 16:15:05 +0900

    Update nCm.cpp

--- Algorithm/nCm.cpp
+++ Algorithm/nCm.cpp
@@ -1,13 +1,13 @@
 
-const int Max = 100;
+const int M = 100;
 typedef long long ll;
 const int mod = 1e9 + 7;
 
 int main() {
-	ll C[Max][Max];
+	ll C[M][M];
 
 	C[0][0] = C[1][1] = C[1][0] = 1;
-	for (int i = 2; i < Max; i++)
+	for (int i = 2; i < M; i++)
 	{
 		for (int j = 0; j <= i; ++j)
 		{

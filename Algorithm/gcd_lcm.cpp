18efa70a87a467804cc52b89d153cb136acc20bb
omu <omuric@users.noreply.github.com>
2016-06-05 16:27:03 +0900

    Update gcd_lcm.cpp

--- Algorithm/gcd_lcm.cpp
+++ Algorithm/gcd_lcm.cpp
@@ -1,2 +1,6 @@
+//最大公約数
 int gcd(int a, int b) { return b ? gcd(b, a%b) : a; }
+//最小公倍数
 int lcm(int a, int b) { return a / gcd(a, b) * b; }
+
+//http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0005&lang=jp

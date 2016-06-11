9393380ce2828c79658ab6d3b20d088a2a278e91
omu <omuric@users.noreply.github.com>
2016-06-11 14:54:27 +0900

    Create minimum_template.cpp

---
+++ Template/minimum_template.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+#define rep(i, j) for(int i = 0; i < j; i++)
+#define all(i) i.begin(), i.end()
+
+using namespace std;
+
+int main()
+{
+	// code
+	
+	return 0;
+}

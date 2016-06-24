b3df171053db39acfcc0990a06411d2f01d47343
omu <omuric@users.noreply.github.com>
2016-06-24 12:10:05 +0900

    Update minimum_template.cpp

--- Template/minimum_template.cpp
+++ Template/minimum_template.cpp
@@ -2,9 +2,13 @@
 #include <algorithm>
 #include <vector>
 #include <string>
+#include <string.h>
+#include <functional>
 
 #define rep(i, j) for(int i = 0; i < j; i++)
 #define all(i) i.begin(), i.end()
+#define each(i,n) for(auto i : n)
+#define clr(n) memset(n,0,sizeof(n))
 
 using namespace std;
 
